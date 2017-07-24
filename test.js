ef803e8e559f23227b835df25c828047fc75aea3
James M Snell <jasnell@gmail.com>
2017-07-23 18:50:56 -0700

    Rename typeof to typeOf in test

--- test.js
+++ test.js
@@ -4,9 +4,9 @@ const Builtin = require('./polyfill');
 const assert = require('assert');
 
 // Verify normal builtins
-assert.strictEqual(Builtin.typeof(Date), 'function');
-assert.strictEqual(Builtin.typeof(new Date()), 'Date');
-assert.strictEqual(Builtin.typeof(Reflect), 'object');
+assert.strictEqual(Builtin.typeOf(Date), 'function');
+assert.strictEqual(Builtin.typeOf(new Date()), 'Date');
+assert.strictEqual(Builtin.typeOf(Reflect), 'object');
 
 class M {
   static [Symbol.builtin]() {
@@ -17,13 +17,13 @@ class M {
 class N {}
 
 // Verify that a class with @@builtin is reported as a builtin
-assert.strictEqual(Builtin.typeof(new M()), 'M');
-assert.strictEqual(Builtin.typeof(M), 'function');
+assert.strictEqual(Builtin.typeOf(new M()), 'M');
+assert.strictEqual(Builtin.typeOf(M), 'function');
 
 // Verify that a class without @@builtin is not reported as a builtin
-assert.strictEqual(Builtin.typeof(new N()), 'object');
-assert.strictEqual(Builtin.typeof(N), 'function');
+assert.strictEqual(Builtin.typeOf(new N()), 'object');
+assert.strictEqual(Builtin.typeOf(N), 'function');
 
 // Verify that we can mask a built-in
 Date[Symbol.builtin] = undefined;
-assert.strictEqual(Builtin.typeof(new Date()), 'object');
+assert.strictEqual(Builtin.typeOf(new Date()), 'object');
