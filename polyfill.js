e27efded44cde17274be77d3849bd76f78d2b283
hejipalanski <hejipalanski@yahoo.com>
2019-04-28 15:21:38 +0800

    changed label to object literal for faster mapping

--- polyfill.js
+++ polyfill.js
@@ -15,7 +15,7 @@ Object.defineProperty(Symbol, 'builtin', {
 
 function GetBuiltinValue(value) {
   if (value === null)
-    return;
+	return;
   const fn = value[kSymbol];
   if (fn === undefined) return;
   const val = fn.call(value);
@@ -25,128 +25,130 @@ function GetBuiltinValue(value) {
 
 function GetOwnBuiltinValue(value) {
   if (value === null)
-    return;
+	return;
   if (!Object.getOwnPropertyDescriptor(value, kSymbol))
-    return;
+	return;
   return GetBuiltinValue(value);
 }
 
 function builtin() {
   if (typeof this !== 'object' && typeof this !== 'function')
-    throw new TypeError('Method invoked on a value that is not an object');
+	throw new TypeError('Method invoked on a value that is not an object');
   return this[kBuiltin];
 }
 
 const AsyncFunction = (async () => {}).constructor;
 const GeneratorFunction = (function*() {}).constructor;
 
-const labels = [
-  Array, 'Array',
-  ArrayBuffer, 'ArrayBuffer',
-  AsyncFunction, 'AsyncFunction',
-  Boolean, 'Boolean',
-  DataView, 'DataView',
-  Date, 'Date',
-  Error, 'Error',
-  EvalError, 'EvalError',
-  Float32Array, 'Float32Array',
-  Float64Array, 'Float64Array',
-  Function, 'function',
-  GeneratorFunction, 'GeneratorFunction',
-  Int8Array, 'Int8Array',
-  Int16Array, 'Int16Array',
-  Int32Array, 'Int32Array',
-  JSON, 'JSON',
-  Map, 'Map',
-  Math, 'Math',
-  Number, 'Number',
-  Object, 'object',
-  Promise, 'Promise',
-  Proxy, 'Proxy',
-  RangeError, 'RangeError',
-  ReferenceError, 'ReferenceError',
-  Reflect, 'Reflect',
-  RegExp, 'RegExp',
-  Set, 'Set',
-  String, 'String',
-  Symbol, 'Symbol',
-  SyntaxError, 'SyntaxError',
-  TypeError, 'TypeError',
-  Uint8Array, 'Uint8Array',
-  Uint8ClampedArray, 'Uint8ClampedArray',
-  Uint16Array, 'Uint16Array',
-  Uint32Array, 'Uint32Array',
-  URIError, 'URIError',
-  WeakMap, 'WeakMap',
-  WeakSet, 'WeakSet'
-];
-for (let n = 0; n < labels.length; n = n + 2) {
-  Object.defineProperties(labels[n], {
-    [kBuiltin]: {
-      enumerable: false,
-      configurable: false,
-      value: labels[n + 1]
-    },
-    [kSymbol]: {
-      enumerable: false,
-      configurable: true,
-      writable: true,
-      value: builtin
-    }});
+const labels = {
+  'Array': Array,
+  'ArrayBuffer': ArrayBuffer,
+  'AsyncFunction': AsyncFunction,
+  'Boolean': Boolean,
+  'DataView' : DataView,
+  'Date': Date,
+  'Error' : Error,
+  'EvalError' : EvalError,
+  'Float32Array': Float32Array,
+  'Float64Array' : Float64Array,
+  'function': Function,
+  'GeneratorFunction' : GeneratorFunction,
+  'Int8Array' : Int8Array,
+  'Int16Array' : Int16Array,
+  'Int32Array' : Int32Array,
+  'JSON' : JSON,
+  'Map' : Map,
+  'Math': Math,
+  'Number' : Number,
+  'object': Object,
+  'Promise' : Promise,
+  'Proxy' : Proxy,
+  'RangeError': RangeError,
+  'ReferenceError' :ReferenceError,
+  'Reflect' : Reflect,
+  'RegExp' : RegExp,
+  'Set' : Set,
+  'String' : String,
+  'Symbol' :Symbol,
+  'SyntaxError' : SyntaxError,
+  'TypeError' : TypeError,
+  'Uint8Array' : Uint8Array,
+  'Uint8ClampedArray' :Uint8ClampedArray,
+  'Uint16Array': Uint16Array,
+  'Uint32Array' :Uint32Array,
+  'URIError': URIError,
+  'WeakMap': WeakMap,
+  'WeakSet' :WeakSet
+};
+
+let type;
+for (type in labels) {
+  Object.defineProperties(labels[type], {
+	[kBuiltin]: {
+	  enumerable: false,
+	  configurable: false,
+	  value: type
+	},
+	[kSymbol]: {
+	  enumerable: false,
+	  configurable: true,
+	  writable: true,
+	  value: builtin
+	}});
 }
 
 if (typeof Atomics !== 'undefined') {
   Object.defineProperties(Atomics, {
-    [kBuiltin]: {
-      enumerable: false,
-      configurable: false,
-      value: 'Atomics'
-    },
-    [kSymbol]: {
-      enumerable: false,
-      configurable: true,
-      writable: true,
-      value: builtin
-    }});
+	[kBuiltin]: {
+	  enumerable: false,
+	  configurable: false,
+	  value: 'Atomics'
+	},
+	[kSymbol]: {
+	  enumerable: false,
+	  configurable: true,
+	  writable: true,
+	  value: builtin
+	}});
 }
 
 if (typeof SharedArrayBuffer !== 'undefined') {
   Object.defineProperties(SharedArrayBuffer, {
-    [kBuiltin]: {
-      enumerable: false,
-      configurable: false,
-      value: 'SharedArrayBuffer'
-    },
-    [kSymbol]: {
-      enumerable: false,
-      configurable: true,
-      writable: true,
-      value: builtin
-    }});
+	[kBuiltin]: {
+	  enumerable: false,
+	  configurable: false,
+	  value: 'SharedArrayBuffer'
+	},
+	[kSymbol]: {
+	  enumerable: false,
+	  configurable: true,
+	  writable: true,
+	  value: builtin
+	}});
 }
 
 function _is(value1, value2) {
   if (typeof value1 !== 'object' && typeof value1 !== 'function')
-    return false;
+	return false;
   const v1 = GetOwnBuiltinValue(value1);
   if (v1 === undefined)
-    return false;
+	return false;
   if (value2 === undefined)
-    return false;
+	return false;
   if (typeof value2 !== 'object' && typeof value1 !== 'function')
-    return false;
+	return false;
   const v2 = GetOwnBuiltinValue(value2);
   return v1 === v2;
 }
 
 function _typeof(value) {
   if (typeof value === 'object' || typeof value === 'function') {
-    const ctor = value.constructor;
-    if (ctor !== undefined) {
-      const val = GetBuiltinValue(ctor);
-      if (val !== undefined)
-        return val;
-    }
+	const ctor = value.constructor;
+	if (ctor !== undefined) {
+	  const val = GetBuiltinValue(ctor);
+	  if (val !== undefined)
+		return val;
+	}
   }
   return typeof value;
 }
@@ -158,16 +160,16 @@ Object.defineProperty(_typeof, 'name', { value: 'typeof' });
 const Builtin = {};
 Object.defineProperties(Builtin, {
   'is': {
-    enumerable: false,
-    configurable: true,
-    writable: true,
-    value: _is
+	enumerable: false,
+	configurable: true,
+	writable: true,
+	value: _is
   },
   'typeof': {
-    enumerable: false,
-    configurable: true,
-    writable: true,
-    value: _typeof
+	enumerable: false,
+	configurable: true,
+	writable: true,
+	value: _typeof
   }
 });
 
